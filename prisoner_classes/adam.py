20f32975ba1198e354b9c727fc1a779d2e101caa
Adam Novotny <adam.novotny@postrock.wall>
2017-11-02 17:59:56 -0400

    Created ImprovedUnderlingTFT

--- prisoner_classes/adam.py
+++ prisoner_classes/adam.py
@@ -18,3 +18,27 @@ class UnderlingTFT(Prisoner):
         if len(opponent_history) == 0 or np.random.uniform(0,1) < self.p:
             return 'C'
         return opponent_history[-1]
+
+class ImprovedUnderlingTFT(Prisoner):
+    def __init__(self):
+        Prisoner.__init__(self)
+        self.name = 'ImprovedUnderling'
+        self.handshake = ['C', 'C', 'D', 'C']
+        self.onlyDefect = ['D', 'D', 'D', 'D', 'D']
+        self.p = 0.05
+
+    def makeDecision(self, opponent_history):
+        if len(opponent_history) == 0:
+            return 'C'
+
+        if len(opponent_history) >= 4:
+            if opponent_history[:4] == self.handshake
+                return 'C'
+
+        if len(opponent_history) <= 5:
+            if opponent_history[:5].count('D') == len(opponent_history)
+                return 'C'
+
+        if np.random.uniform(0,1) < self.p:
+            return 'C'
+        return opponent_history[-1]
