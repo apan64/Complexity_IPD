8d7a59d55adae5c77585cb027cb04af35846dfe4
Apurva Raman <apurva.raman@students.olin.edu>
2017-11-03 00:00:30 -0400

    Adding Goldfish class.

--- prisoner_classes/base_class.py
+++ prisoner_classes/base_class.py
@@ -5,24 +5,50 @@ class Prisoner:
         self.history = []
         # TODO: Give your class a name for me to refer to it by
         self.name = 'Default'
-        
+
     def getHistory(self):
         return self.history
-    
+
     def addToHistory(self, decision):
         self.history.append(decision)
         return
-    
+
     def makeDecision(self, history):
         '''
         Input: List containing the history of the opposing agent's decisions throughout previous games
-        
+
         Output: The character 'C' or 'D', to represent the agent's choice to either cooperate or defect
         '''
         # TODO: Overwrite this function yourself!
         return 'D'
-    
+
     def playDilemma(self, opponent):
         decision = self.makeDecision(opponent.getHistory())
         self.addToHistory(decision)
         return decision
+
+class Goldfish(Prisoner):
+    def __init__(self):
+        Prisoner.__init__(self)
+        self.name = 'Goldfish'
+
+    def decideOnPrev(self, myarr, t):
+        defect = 0
+        for i in myarr:
+            if i == 'D':
+                defect += 1
+                if (defect > t):
+                    return 'D'
+                else:
+                    return 'C'
+
+    def makeDecision(self, history):
+        if (len(history) > 10):
+            last10 = history[-9:]
+            return self.decideOnPrev(last10, 3)
+
+        elif (len(history) == 0):
+            return 'C'
+
+        else:
+            return self.decideOnPrev(history, len(history)//2)
